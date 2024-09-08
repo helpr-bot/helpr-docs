569efd9235f9b5b2b176e086c0ee58e12494816d
hello391 <mmaaxxmmaaxxaa@gmail.com>
2024-09-07 21:47:47 -0400

    inital commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,109 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "Helpr Docs",
+  tagline: "Coming Soon!",
+  favicon: "https://helpr.gg/icon.ico",
+
+  // Set the production url of your site here
+  url: "https://docs.helpr.gg",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "helpr-bot", // Usually your GitHub org/user name.
+  projectName: "helpr-docs", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  trailingSlash: false,
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/helpr-bot/docs",
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    navbar: {
+      title: "Helpr Docs",
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Docs",
+        },
+
+        {
+          href: "https://github.com/helpr-bot/docs",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Intro",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Discord",
+              href: "https://helpr.gg/support",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "GitHub",
+              href: "https://github.com/helpr-bot/docs",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Helpr. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import Heading from "@theme/Heading";
+
+import styles from "./index.module.css";
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
+      <div className={styles.main}>
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+      </div>
+      <div className={styles.sub}>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
+      <div className={styles.buttons}>
+        <Link className="button button--secondary button--lg" to="/docs/intro">
+          Jump to Docs
+        </Link>
+      </div>
+    </Layout>
+  );
+}
